da53dadccaab5d3bdd0ee02f3bc5e1dcb302c6b0
ramennoodlesxv <94365980+RamuneNeptune@users.noreply.github.com>
2023-05-06 00:15:11 +1000

    ModulesMod, added vehicle handlers

---
+++ SharedPlugins/ModulesMod/Handlers/ExosuitHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DRS.ModulesMod.Handlers
+{
+    public class ExosuitHandler : MonoBehaviour
+    {
+        public static List<GameObject> exosuits = new List<GameObject>();
+        public Exosuit exosuit;
+
+        public void OnEnable() => exosuits.Add(gameObject);
+        public void OnDisable() => exosuits.Remove(gameObject);
+
+        public void Start()
+        {
+            exosuit = gameObject.GetComponentInChildren<Exosuit>();
+        }
+    }
+}
---
+++ SharedPlugins/ModulesMod/Handlers/SeamothHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DRS.ModulesMod.Handlers
+{
+    public class SeamothHandler : MonoBehaviour
+    {
+        public static List<GameObject> seamoths = new List<GameObject>();
+        public SeaMoth seamoth;
+
+        public void OnEnable() => seamoths.Add(gameObject);
+        public void OnDisable() => seamoths.Remove(gameObject);
+
+        public void Start()
+        {
+            seamoth = gameObject.GetComponentInChildren<SeaMoth>();
+        }
+    }
+}
