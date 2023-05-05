0e101003e23f2520717e5dc58ae1683510ba0953
ramennoodlesxv <94365980+RamuneNeptune@users.noreply.github.com>
2023-05-05 16:27:15 +1000

    ModulesMod (placeholder name) initial commit

---
+++ SharedPlugins/ModulesMod/BepInEx.cs
@@ -0,0 +1,25 @@
+﻿
+using BepInEx.Logging;
+using BepInEx;
+using HarmonyLib;
+
+namespace DRS.ModulesMod
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    [BepInProcess("Subnautica.exe")]
+    public class ModulesMod : BaseUnityPlugin
+    {
+        private const string myGUID = "com.drs.ModulesMod";
+        private const string pluginName = "ModulesMod";
+        private const string versionString = "1.0.0";
+        private static readonly Harmony harmony = new Harmony(myGUID);
+        public static ManualLogSource logger;
+
+        public void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "has breached the mainframe.. successfully loaded");
+            logger = Logger;
+        }
+    }
+}
