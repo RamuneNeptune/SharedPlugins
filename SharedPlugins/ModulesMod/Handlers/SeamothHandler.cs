d276a8a147982c18fc7486d97d058d52a4bc03e8
ramennoodlesxv <94365980+RamuneNeptune@users.noreply.github.com>
2023-05-06 12:25:10 +1000

    ModulesMod, updates to SeamothHandler

--- SharedPlugins/ModulesMod/Handlers/SeamothHandler.cs
+++ SharedPlugins/ModulesMod/Handlers/SeamothHandler.cs
@@ -9,18 +9,41 @@ namespace DRS.ModulesMod.Handlers
 {
     public class SeamothHandler : MonoBehaviour
     {
-        public SeaMoth seamoth;
+        public object[] lastVehicleValues;
+        public EngineRpmSFXManager engine;
+        public EnergyMixin energyMixin;
         public Animator animator;
         public Vehicle vehicle;
-        
+        public SeaMoth seamoth;
+
         public void Start()
         {
+            engine = gameObject.GetComponentInChildren<EngineRpmSFXManager>();
             seamoth = gameObject.GetComponentInChildren<SeaMoth>();
-            animator = gameObject.GetComponent<Animator>();
-            vehicle = gameObject.GetComponent<Vehicle>();
+            animator = gameObject.GetComponentInChildren<Animator>();
+            vehicle = gameObject.GetComponentInParent<Vehicle>();
+
+            lastVehicleValues = new object[] 
+            {
+                vehicle.forwardForce,
+                vehicle.backwardForce,
+                vehicle.sidewardForce,
+                seamoth.enginePowerConsumption,
+                engine.engineRpmSFX,
+                animator.speed,
+            };
         }
 
         public void SetLastValues()
+        {
+            lastVehicleValues[0] = vehicle.forwardForce;
+            lastVehicleValues[1] = vehicle.backwardForce;
+            lastVehicleValues[2] = vehicle.sidewardForce;
+            lastVehicleValues[3] = seamoth.enginePowerConsumption;
+            lastVehicleValues[5] = animator.speed;
+        }
+
+        public void Update()
         {
 
         }
