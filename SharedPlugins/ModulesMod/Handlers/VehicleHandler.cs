2d6a75974a34c101a840ae30b60a13cdb8b4e878
SamuramongeDev <giovandres1325@gmail.com>
2023-05-06 18:37:51 -0300

    ModulesMod, created a VehicleHandler abstract class
    Created an abstract class for vehicle upgrade handling, containing the basics for module handling.

---
+++ SharedPlugins/ModulesMod/Handlers/VehicleHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DRS.ModulesMod.Handlers
+{
+    public abstract class VehicleHandler : MonoBehaviour
+    {
+        public Vehicle vehicle;
+
+        public virtual void Awake()
+        {
+            vehicle = GetComponentInParent<Vehicle>();
+
+            vehicle.modules.onEquip += (slot, item) => 
+            {
+                OnModuleEquip(item.techType, vehicle.modules.GetCount(item.techType));
+            };
+
+            vehicle.modules.onUnequip += (slot, item) => 
+            {
+                OnModuleUnequip(item.techType, vehicle.modules.GetCount(item.techType));
+            };
+
+            vehicle.onToggle += (slotid, state) =>
+            {
+                OnModuleToggle(vehicle.GetSlotBinding(slotid), slotid, state);
+            };
+        }
+
+
+
+        public virtual void OnModuleEquip(TechType techType, int count) { }
+        public virtual void OnModuleUnequip(TechType techType, int count) { }
+        public virtual void OnModuleToggle(TechType techType, int slotID, bool state) { }
+    }
+}
