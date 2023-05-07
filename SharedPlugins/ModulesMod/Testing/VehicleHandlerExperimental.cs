1353125f2c67e9c412ce8c0b5210449d750ee643
SamuramongeDev <giovandres1325@gmail.com>
2023-05-06 21:43:52 -0300

    ModulesMod, updated VehicleHandlerExperimental.cs
    Updated the script.

--- SharedPlugins/ModulesMod/Testing/VehicleHandlerExperimental.cs
+++ SharedPlugins/ModulesMod/Testing/VehicleHandlerExperimental.cs
@@ -1,52 +1,43 @@
-﻿using System;
+﻿using Nautilus.Assets;
+using System;
 using System.Collections.Generic;
 
 namespace DRS.ModulesMod.Testing
 {
     public class VehicleHandlerExperimental
     {
-        // Example 1: With Module
-        public void Example1(TechType techType)
-        {
-            ModuleHandler.moduleTree[Helpers.VehicleType.Seamoth][techType].OnEquip.Invoke();
-        }
-
-        // Example 2: With MinimalModule
-        public void Example2(TechType techType)
-        {
-            ModuleHandler.minimalModuleTree[Helpers.VehicleType.Seamoth][techType].OnEquip.Invoke();
-        }
-
-        // Example 3: With List<Module>
-        public void Example3(TechType techType)
-        {
-            var index = ModuleHandler.GetIndex(Helpers.VehicleType.Seamoth, techType);
-            ModuleHandler.moduleListTree[Helpers.VehicleType.Seamoth][index].OnEquip.Invoke();
-        }
     }
 
-    public class ModuleHandler
+    public class ModuleHandler // Combine with Helpers
     {
-        public static readonly Dictionary<Helpers.VehicleType, Dictionary<TechType, Module>> moduleTree = new Dictionary<Helpers.VehicleType, Dictionary<TechType, Module>>();
-        
-        // For testing, see wich one do you like, i prefer this one below.
-        public static readonly Dictionary<Helpers.VehicleType, Dictionary<TechType, MinimalModule>> minimalModuleTree = new Dictionary<Helpers.VehicleType, Dictionary<TechType, MinimalModule>>();
+        public static readonly Dictionary<Helpers.VehicleType, List<Module>> moduleTree = new Dictionary<Helpers.VehicleType, List<Module>>();
 
-        // And this one
-        public static readonly Dictionary<Helpers.VehicleType, List<Module>> moduleListTree = new Dictionary<Helpers.VehicleType, List<Module>>();
-
-        public static int GetIndex(Helpers.VehicleType vehicleType, TechType techType)
+        public static void AddModule(Module module)
         {
-            int result = 0;
-            int count = 0;
-            moduleListTree[vehicleType].ForEach(module =>
-            {
-                if (module.techType == techType)
-                    result = count;
-                count++; 
-            });
-            return result;
+            if (!moduleTree[module.vehicleType].Contains(module))
+                moduleTree[module.vehicleType].Add(module);
         }
+
+        public static Module GetModule(Helpers.VehicleType vehicleType, TechType techType)
+        {
+            foreach (var module in moduleTree[vehicleType])
+                if (module.techType == techType) return module;
+            return default;
+        }
+
+        // maybe change to PrefabInfo instead of TechType?
+        public static Module CreateModule(TechType techType, Helpers.VehicleType vehicleType, Action OnEquip = null, Action OnUnequip = null, Action<bool> OnToggle = null)
+        {
+            Module module = new Module
+            {
+                techType = techType,
+                vehicleType = vehicleType,
+                OnEquip = OnEquip,
+                OnUnequip = OnUnequip,
+                OnToggle = OnToggle
+            };
+            return module;
+        } 
     }
 
     public struct Module
@@ -59,93 +50,7 @@ namespace DRS.ModulesMod.Testing
         public Action<bool> OnToggle; // Ideally Action<VehicleHandler, bool>
 
         /// <summary>
-        /// Creates a module with no events.
-        /// </summary>
-        /// <param name="techType">The TechType of the module.</param>
-        /// <param name="vehicleType">The vehicle the module belongs on.</param>
-        public Module(TechType techType, Helpers.VehicleType vehicleType)
-        {
-            this.techType = techType;
-            this.vehicleType = vehicleType;
-
-            OnEquip = new Action( () => { } );
-            OnUnequip = new Action( () => { } );
-            OnToggle = new Action<bool>( @bool => { } );
-        }
-
-        /// <summary>
-        /// Creates a module with an:
-        /// <para>OnEquip event: An action that will be called each time this module is equiped on the vehicle.</para>
-        /// </summary>
-        /// <param name="techType">The TechType of the module.</param>
-        /// <param name="vehicleType">The vehicle the module belongs on.</param>
-        /// <param name="onEquip">The Action that will be executed each time this module is equiped on the vehicle.</param>
-        public Module(TechType techType, Helpers.VehicleType vehicleType, Action onEquip)
-        {
-            this.techType = techType;
-            this.vehicleType = vehicleType;
-
-            OnEquip = onEquip;
-            OnUnequip = new Action(() => { });
-            OnToggle = new Action<bool>(@bool => { });
-        }
-
-        /// <summary>
-        /// Creates a module with an:
-        /// <para>OnToggle event: An action that will be called each time this module is toggled. (In vehicle slots)</para>
-        /// </summary>
-        /// <param name="techType">The TechType of the module.</param>
-        /// <param name="vehicleType">The vehicle the module belongs on.</param>
-        /// <param name="onToggle">The action that will be execited each time the module is toggled.</param>
-        public Module(TechType techType, Helpers.VehicleType vehicleType, Action<bool> onToggle)
-        {
-            this.techType = techType;
-            this.vehicleType = vehicleType;
-
-            OnEquip = new Action(() => { });
-            OnUnequip = new Action(() => { });
-            OnToggle = onToggle;
-        }
-        /// <summary>
-        /// Creates a module with an:
-        /// <para>OnEquip event: An action that will be called each time this module is equiped on the vehicle.</para>
-        /// <para>OnToggle event: An action that will be called each time this module is toggled. (In vehicle slots)</para>
-        /// </summary>
-        /// <param name="techType">The TechType of the module.</param>
-        /// <param name="vehicleType">The vehicle the module belongs on.</param>
-        /// <param name="onEquip">The Action that will be executed each time this module is equiped on the vehicle.</param>
-        /// <param name="onToggle">The action that will be execited each time the module is toggled.</param>
-        public Module(TechType techType, Helpers.VehicleType vehicleType, Action onEquip, Action<bool> onToggle)
-        {
-            this.techType = techType;
-            this.vehicleType = vehicleType;
-
-            OnEquip = onEquip;
-            OnUnequip = new Action(() => { });
-            OnToggle = onToggle;
-        }
-
-        /// <summary>
-        /// Creates a module with an:
-        /// <para>OnEquip event: An action that will be called each time this module is equiped on the vehicle.</para>
-        /// <para>OnUnequip event: An action that will be called each time this module is unequiped from the vehicle. (In vehicle slots)</para>
-        /// </summary>
-        /// <param name="techType">The TechType of the module.</param>
-        /// <param name="vehicleType">The vehicle the module belongs on.</param>
-        /// <param name="onEquip">The Action that will be executed each time this module is equiped on the vehicle.</param>
-        /// <param name="onUnequip">The Action that will be executed each time this module is unequiped from the vehicle.</param>
-        public Module(TechType techType, Helpers.VehicleType vehicleType, Action onEquip, Action onUnequip)
-        {
-            this.techType = techType;
-            this.vehicleType = vehicleType;
-
-            OnEquip = onEquip;
-            OnUnequip = onUnequip;
-            OnToggle = new Action<bool>(@bool => { });
-        }
-
-        /// <summary>
-        /// Creates a module with an:
+        /// Creates a module with an (optional):
         /// <para>OnEquip event: An action that will be called each time this module is equiped on the vehicle.</para>
         /// <para>OnUnequip event: An action that will be called each time this module is unequiped from the vehicle. (In vehicle slots)</para>
         /// <para>OnToggle event: An action that will be called each time this module is toggled. (In vehicle slots)</para>
@@ -155,85 +60,11 @@ namespace DRS.ModulesMod.Testing
         /// <param name="onEquip">The Action that will be executed each time this module is equiped on the vehicle.</param>
         /// <param name="onUnequip">The Action that will be executed each time this module is unequiped from the vehicle.</param>
         /// <param name="onToggle">The action that will be execited each time the module is toggled.</param>
-        public Module(TechType techType, Helpers.VehicleType vehicleType, Action onEquip, Action onUnequip, Action<bool> onToggle)
+        public Module(Helpers.VehicleType vehicleType, TechType techType, Action onEquip = null, Action onUnequip = null, Action<bool> onToggle = null)
         {
             this.techType = techType;
             this.vehicleType = vehicleType;
 
-            OnEquip = onEquip;
-            OnUnequip = onUnequip;
-            OnToggle = onToggle;
-        }
-    }
-
-    public struct MinimalModule
-    {
-        public Action OnEquip; // Ideally Action<VehicleHandler>
-        public Action OnUnequip; // Ideally Action<VehicleHandler>
-        public Action<bool> OnToggle; // Ideally Action<VehicleHandler, bool>
-
-        /// <summary>
-        /// Creates a module with an:
-        /// <para>OnEquip event: An action that will be called each time this module is equiped on the vehicle.</para>
-        /// </summary>
-        /// <param name="onEquip">The Action that will be executed each time this module is equiped on the vehicle.</param>
-        public MinimalModule(Action onEquip)
-        {
-            OnEquip = onEquip;
-            OnUnequip = new Action(() => { });
-            OnToggle = new Action<bool>(@bool => { });
-        }
-
-        /// <summary>
-        /// Creates a module with an:
-        /// <para>OnToggle event: An action that will be called each time this module is toggled. (In vehicle slots)</para>
-        /// </summary>
-        /// <param name="onToggle">The action that will be execited each time the module is toggled.</param>
-        public MinimalModule(Action<bool> onToggle)
-        {
-            OnEquip = new Action(() => { });
-            OnUnequip = new Action(() => { });
-            OnToggle = onToggle;
-        }
-        /// <summary>
-        /// Creates a module with an:
-        /// <para>OnEquip event: An action that will be called each time this module is equiped on the vehicle.</para>
-        /// <para>OnToggle event: An action that will be called each time this module is toggled. (In vehicle slots)</para>
-        /// </summary>
-        /// <param name="onEquip">The Action that will be executed each time this module is equiped on the vehicle.</param>
-        /// <param name="onToggle">The action that will be execited each time the module is toggled.</param>
-        public MinimalModule(Action onEquip, Action<bool> onToggle)
-        {
-            OnEquip = onEquip;
-            OnUnequip = new Action(() => { });
-            OnToggle = onToggle;
-        }
-
-        /// <summary>
-        /// Creates a module with an:
-        /// <para>OnEquip event: An action that will be called each time this module is equiped on the vehicle.</para>
-        /// <para>OnUnequip event: An action that will be called each time this module is unequiped from the vehicle. (In vehicle slots)</para>
-        /// </summary>
-        /// <param name="onEquip">The Action that will be executed each time this module is equiped on the vehicle.</param>
-        /// <param name="onUnequip">The Action that will be executed each time this module is unequiped from the vehicle.</param>
-        public MinimalModule(Action onEquip, Action onUnequip)
-        {
-            OnEquip = onEquip;
-            OnUnequip = onUnequip;
-            OnToggle = new Action<bool>(@bool => { });
-        }
-
-        /// <summary>
-        /// Creates a module with an:
-        /// <para>OnEquip event: An action that will be called each time this module is equiped on the vehicle.</para>
-        /// <para>OnUnequip event: An action that will be called each time this module is unequiped from the vehicle. (In vehicle slots)</para>
-        /// <para>OnToggle event: An action that will be called each time this module is toggled. (In vehicle slots)</para>
-        /// </summary>
-        /// <param name="onEquip">The Action that will be executed each time this module is equiped on the vehicle.</param>
-        /// <param name="onUnequip">The Action that will be executed each time this module is unequiped from the vehicle.</param>
-        /// <param name="onToggle">The action that will be execited each time the module is toggled.</param>
-        public MinimalModule(Action onEquip, Action onUnequip, Action<bool> onToggle)
-        {
             OnEquip = onEquip;
             OnUnequip = onUnequip;
             OnToggle = onToggle;
